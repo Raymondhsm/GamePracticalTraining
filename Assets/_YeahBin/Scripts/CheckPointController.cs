1036d37c822e3105c2c05cd5fef7b1346c4488da
Raymondhsm <1260307197@qq.com>
2020-04-23 23:10:39 +0800

    3

---
+++ Assets/_YeahBin/Scripts/CheckPointController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  m_target 放置触发的结果
+/// </summary>
+public class CheckPointController : MonoBehaviour
+{
+    private AudioSource m_Audios;      //音频源
+    public AudioClip CheckPoint_Audio;
+
+    public GameObject[] CheckPoints;//下一个/组要显示的关卡点
+    private Transform[] m_Child;//用来暂存的
+
+    void Start()
+    {
+        m_Audios = GetComponent<AudioSource>();
+        m_Audios.clip = CheckPoint_Audio;
+
+        for (int i=0;i< CheckPoints.Length;i++)
+        {
+            CheckPoints[i].GetComponent<MeshRenderer>().enabled = false; 
+            CheckPoints[i].GetComponent<Collider>().enabled = false;
+
+            //对子物体遍历
+            m_Child = CheckPoints[i].GetComponentsInChildren<Transform>();
+            foreach (Transform child in m_Child)
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                if(child.gameObject.GetComponent<Collider>())
+                child.gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            m_Audios.PlayOneShot(CheckPoint_Audio);
+
+            //对自己
+            this.GetComponent<MeshRenderer>().enabled = false;
+            this.GetComponent<Collider>().enabled = false;
+
+            //对子物体遍历
+            m_Child = this.GetComponentsInChildren<Transform>();
+            foreach (Transform child in m_Child)
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            //Destroy(gameObject);
+
+
+            //对下一个/组要显示的关卡点
+            for (int i = 0; i < CheckPoints.Length; i++)
+            {
+                CheckPoints[i].GetComponent<MeshRenderer>().enabled = true;
+                CheckPoints[i].GetComponent<Collider>().enabled = true;
+
+                //对子物体遍历
+                m_Child = CheckPoints[i].GetComponentsInChildren<Transform>();
+                foreach (Transform child in m_Child)
+                {
+                    child.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_YeahBin/Scripts/Ground2show.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground2show : MonoBehaviour
+{
+    public GameObject Ground;
+
+    public float speed = 10.0f;
+    public Vector3 endPoint = Vector3.zero;
+    public bool pingpong = true;
+
+    private Vector3 curEndPoint = Vector3.zero;
+    private Vector3 startPoint = Vector3.zero;
+    private bool showGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showGround = false;
+        startPoint = Ground.transform.position;
+        curEndPoint = endPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showGround)
+        {
+            Ground.transform.position = Vector3.Slerp(Ground.transform.position, curEndPoint, Time.deltaTime * speed);
+            if (pingpong)
+            {
+                if (Vector3.Distance(Ground.transform.position, curEndPoint) < 0.1f)
+                {
+                    //Debug.Log("curEndPoint.y"+ curEndPoint.y);
+                    curEndPoint = Vector3.Distance(curEndPoint, endPoint) < Vector3.Distance(curEndPoint, startPoint) ? startPoint : endPoint;
+                }
+            }
+        }
+           
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            showGround = true;
+            this.GetComponent<MeshRenderer>().enabled = false;
+            this.GetComponent<Collider>().enabled = false;
+        }
+    }
+}
